67d4f663ba0e0abaa166cc2e2bc38a6af016cf8b
hoo091221 <hoo091221@naver.com>
2026-09-10 01:31:10 +0900

    버그수정2

--- src/Pages/MusicPlayer.jsx
+++ src/Pages/MusicPlayer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 
-const AUDIO_SRC = './src/sounds/background.mp3';
+const AUDIO_SRC = 'background.mp3';
 
 export default function MusicPlayer({ isPlaying, setIsPlaying, shouldMute }) {
   const audioRef = useRef(null);
